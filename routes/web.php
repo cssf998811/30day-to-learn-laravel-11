107a262013802029149d32ed72f9dcc3dc7495c2
cssf998811 <cssf998811@gmail.com>
2024-03-15 03:02:15 +0800

    Day2 HW:Create a /contact route, and load a corresponding view.

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+//    return 'About Page';
+//    return ['foo' => 'bar'];
+    return view('about');
+});
+
+Route::get('/contact', function () {
+//    return 'About Page';
+//    return ['foo' => 'bar'];
+    return view('contact');
+});
